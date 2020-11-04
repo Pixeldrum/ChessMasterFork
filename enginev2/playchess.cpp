1cd2effa73075eee6670925d8185db7a6a71911c
Pixeldrum <56083084+Pixeldrum@users.noreply.github.com>
2020-11-04 18:03:04 -0500

    Add files via upload

---
+++ enginev2/playchess.cpp
@@ -0,0 +1,67 @@
+//#include <mcheck.h>
+#include <cstdlib>
+#include <cstdio>
+#include <list>
+#include "chessboard.h"
+#include "humanplayer.h"
+#include "aiplayer.h"
+
+using namespace std;
+
+int main(void) {
+
+	ChessBoard board;
+	list<Move> regulars, nulls;
+	int turn = WHITE;
+	Move move;
+	bool found;
+
+	// Initialize players
+	AIPlayer black(BLACK, 3);
+	HumanPlayer white(WHITE);
+
+	// setup board
+	board.initDefaultSetup();
+
+	for(;;) {
+		// show board
+		board.print();
+
+		// query player's choice
+		if(turn)
+			found = black.getMove(board, move);
+		else
+			found = white.getMove(board, move);
+
+		if(!found)
+			break;
+
+		// if player has a move get all moves
+		regulars.clear();
+		nulls.clear();
+		board.getMoves(turn, regulars, regulars, nulls);
+
+		// execute maintenance moves
+		for(list<Move>::iterator it = nulls.begin(); it != nulls.end(); ++it)
+			board.move(*it);
+
+		// execute move
+		board.move(move);
+		move.print();
+
+		// opponents turn
+		turn = TOGGLE_COLOR(turn);
+	}
+
+	ChessPlayer::Status status = board.getPlayerStatus(turn);
+
+	switch(status)
+	{
+		case ChessPlayer::Checkmate:
+			printf("Checkmate\n");
+			break;
+		case ChessPlayer::Stalemate:
+			printf("Stalemate\n");
+			break;
+	}
+}
